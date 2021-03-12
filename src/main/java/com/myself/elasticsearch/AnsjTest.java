185ba58fc588e81fe431be473fe5dcf94c0f0b3a
xuzhaoju <xzj-work@139.com>
2021-03-12 15:51:59 +0800

    111

--- src/main/java/com/myself/elasticsearch/AnsjTest.java
+++ src/main/java/com/myself/elasticsearch/AnsjTest.java
@@ -31,7 +31,8 @@ public class AnsjTest {
             add("userDefine");
             add("wh");
         }};
-        String str = "欢迎使用ansj_seg,(ansj中文分词)在这里如果你遇到什么问题都可以联系我.我一定尽我所能.帮助大家.ansj_seg更快,更准,更自由!";
+//        String str = "欢迎使用ansj_seg,(ansj中文分词)在这里如果你遇到什么问题都可以联系我.我一定尽我所能.帮助大家.ansj_seg更快,更准,更自由!";
+        String str = "证明2014年02月05日12时23分在周浦镇里仁村6组横桥路上";
         Result result = ToAnalysis.parse(str); //分词结果的一个封装，主要是一个List<Term>的terms
         System.out.println(result.getTerms());
 
